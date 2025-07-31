d7764d53b1fd5bf47dc3f62d71821972ceac5527
rikka <1252543514@qq.com>
2025-08-01 01:00:34 +0800

    creat: LVGL移植完成，测试正常

--- ESP32_LCD_ST7789/src/ui/ui.h
+++ ESP32_LCD_ST7789/src/ui/ui.h
@@ -1,11 +1,11 @@
-////
-//// Created by Yuta on 2025/7/31.
-////
 //
-//#ifndef ESP32_LCD_ST7789_UI_H
-//#define ESP32_LCD_ST7789_UI_H
+// Created by Yuta on 2025/7/31.
 //
-//void ui_init(void);
-//void ui_update(void);
-//
-//#endif //ESP32_LCD_ST7789_UI_H
+
+#ifndef ESP32_LCD_ST7789_UI_H
+#define ESP32_LCD_ST7789_UI_H
+
+void ui_init();
+void ui_update();
+
+#endif //ESP32_LCD_ST7789_UI_H
